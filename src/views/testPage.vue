97e1cfe18f8b6a05eb54cc75ecf041f2cc84f503
“Instser” <1027691813@qq.com>
2024-04-12 20:37:58 +0800

    .

---
+++ src/views/testPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+
+axios.get('/api/quesFolder/getFolder').then(res => {
+  console.log(res.data.data)
+  text.value = res.data.data
+})
+const text = ref('默认')
+
+</script>
+
+<template>
+<p>{{ text }}</p>
+</template>
+
+<style scoped>
+
+</style>
